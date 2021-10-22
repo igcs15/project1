46cef6089f51c5f4020c22469da3be88b024c192
igcs15 <80236172+igcs15@users.noreply.github.com>
2021-10-22 09:57:30 +0530

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   resources :line_items
   resources :carts
   root 'store#index', as: 'store_index'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
